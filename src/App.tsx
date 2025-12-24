f54aee6b35b92a5bdd57bd4fae495bab304fca92
Jorge Roberto Argolo <jorgerobertoargolo@gmail.com>
2025-12-24 08:55:02 -0300

    config: configurando router

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,14 @@
 import { ThemeProvider } from 'styled-components';
 import { BrowserRouter } from 'react-router-dom'
 import { defaultTheme } from './styles/themes/default';
+import { Router } from './Router';
 
 export function App() {
   return (
     <ThemeProvider theme={defaultTheme}>
       <BrowserRouter>
         {/* Aqui ficará o Contexto principal*/}
-        <>
-          <>{/* Aqui ficarão as rotas */}</>
-        </>
+          <Router />
       </BrowserRouter>
     </ThemeProvider>
   )
