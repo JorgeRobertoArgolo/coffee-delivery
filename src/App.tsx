bfbac2b90e1bcf45e73bdab1a4cf52b1cbd06d57
Jorge Roberto Argolo <jorgerobertoargolo@gmail.com>
2025-12-24 08:42:55 -0300

    config: Configurando o styled-components, as cores e baixando fontes que serão usadas

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,16 @@
+import { ThemeProvider } from 'styled-components';
+import { BrowserRouter } from 'react-router-dom'
+import { defaultTheme } from './styles/themes/default';
+
 export function App() {
   return (
-    <>
-      <h1>Coffee Delivery</h1>
-    </>
+    <ThemeProvider theme={defaultTheme}>
+      <BrowserRouter>
+        {/* Aqui ficará o Contexto principal*/}
+        <>
+          <>{/* Aqui ficarão as rotas */}</>
+        </>
+      </BrowserRouter>
+    </ThemeProvider>
   )
 }
